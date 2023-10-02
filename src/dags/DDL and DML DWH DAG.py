9960ef4283f5e3a7390fc91fd90ba125b4dc886a
MarlonGando <104733109+MarlonGando@users.noreply.github.com>
2023-10-03 00:50:27 +0300

    Update DDL and DML DWH DAG.py
     Убрал закомиченные куски кода

--- src/dags/DDL and DML DWH DAG.py
+++ src/dags/DDL and DML DWH DAG.py
@@ -8,11 +8,6 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 
-
-#AWS_ACCESS_KEY_ID = "YCAJEWXOyY8Bmyk2eJL-hlt2K"
-#AWS_SECRET_ACCESS_KEY = "YCPs52ajb2jNXxOUsL4-pFDL1HnV2BCPd928_ZoA"
-
-
 conn_info = {'host': 'vertica.tgcloudenv.ru', 
              'port': '5433',
              'user': 'stv2023081266',       
@@ -20,26 +15,6 @@ conn_info = {'host': 'vertica.tgcloudenv.ru',
              'database': 'dwh',
              'autocommit': True
 }
-
-'''f'src/sql/DDL DWH objects.sql'
-f'src/sql/DML DWH data.sql'
-
-def ddl(table_schema: str, suffix: str):
-    with vertica_python.connect(**conn_info) as conn:
-        cur = conn.cursor()
-        script_name = f'src/sql/DDL DWH objects.sql'
-        cur.execute(open(script_name, 'r').read())
-    log.info(f'Проверка {table_schema}{suffix} завершена успешно')
-
-def load_to_dwh(table_schema: str, suffix: str):
-    """Функция принимает наименование схемы и суффикс типа таблицы
-        и загружает данные в таблицу"""
-    with vertica_python.connect(**conn_info) as conn:
-        cur = conn.cursor()
-        script_name = f'src/sql/loaddata_{table_schema}{suffix}.sql'
-        cur.execute(open(script_name, 'r').read())
-    log.info(f'Данные загружены в таблицы {table_schema}{suffix}')'''
-
 
 def fetch_sql_query_from_a_file(path: str):
     with open(path) as file:
@@ -57,9 +32,6 @@ def run_sql_query(query: str):
 def worker(file):
     directory = os.path.join(pathlib.Path(__file__).parent.absolute(), 'sql')
     path = f'{directory}/{file}'
-    
-    #path = f'C:/Users/Grek/Desktop/Practicum/Sprint 6/de-project-sprint-6/src/sql/{file}'
-    #path = f'src/sql/{file}'
     query = fetch_sql_query_from_a_file(path)
     run_sql_query(query)
 
@@ -80,7 +52,6 @@ DDL = PythonOperator(task_id='DDL',
 DML = PythonOperator(task_id='DML',
                      python_callable=worker,
                      op_kwargs={'file': 'DML_DWH_data.sql'},
-                     #op_kwargs={'file': 'src/sql/DML DWH data.sql'},
                      dag=dag)
 
 DDL >> DML
